098a20823c4bd3f254c86e4e6653639be1bd7354
Lachy Richards <lachyrichards@Lachys-MacBook-Pro.local>
2023-09-22 07:19:51 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `lr-photos-23`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "yl5h5md4",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
